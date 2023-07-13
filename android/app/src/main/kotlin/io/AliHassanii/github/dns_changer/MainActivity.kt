7a2039489ccd0e26191eba998b8550944625680c
Ali-Hassanii <Ali-A.M.Hassani@outlook.com>
2023-07-13 17:54:51 +0000

    initial flutter app

---
+++ android/app/src/main/kotlin/io/AliHassanii/github/dns_changer/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.AliHassanii.github.dns_changer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(AppBar());
+
+class AppLogic extends StatelessWidget {
+  const AppLogic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
